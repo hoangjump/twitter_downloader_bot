fe2cf4be3873fb5975d5bc7ff7bf8305d5d3ce99
Văn Hoàng <85290689+hoangjump@users.noreply.github.com>
2024-04-22 15:44:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "6969844941:AAEhLd8boElywJZhvjlTz3w9u4mpfGsXqeM"  # Your telegram bot token
+BOT_TOKEN = "6969844941:AA3w9u4mpfGsXqeM"  # Your telegram bot token
 DEVELOPER_ID = 980182231  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
